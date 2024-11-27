2e4e8d0ecffb0b5264e285b344adcbf92533eafc
someone <85572601+paulhuynh1607@users.noreply.github.com>
2024-11-26 21:42:34 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,5 +1,21 @@
 import pygame
 
+SHAPES = [
+    [[1, 1, 1],
+     [0, 1, 0]],  # T-shape
+    [[1, 1],
+     [1, 1]],  # Square
+    [[1, 1, 1, 1]],  # Line
+    [[1, 1, 0],
+     [0, 1, 1]],  # Z-shape
+    [[0, 1, 1],
+     [1, 1, 0]],  # S-shape
+    [[1, 0, 0],
+     [1, 1, 1]],  # L-shape
+    [[0, 0, 1],
+     [1, 1, 1]]  # J-shape
+]
+
 
 class Blocks:
     def __init__(self):
@@ -7,11 +23,11 @@ class Blocks:
         self.blockX = 29
         self.rect = pygame.Rect(150, 30, self.blockX, self.blockY)
 
-    def create(self, screen):
+    def draw(self, screen):
         pygame.draw.rect(screen, "blue", self.rect)
 
     def falling(self, speed):
-        if self.rect.y != 600:
+        if self.rect.y != 570:
             self.rect.move_ip(0, speed)
         else:
             self.rect.x = 150
@@ -24,4 +40,3 @@ class Blocks:
     def right(self, speed):
         if self.rect.x != 270:
             self.rect.move_ip(speed, 0)
-
