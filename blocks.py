c2e4d2234327d6af32f0c5afb1688d0c6e2f16c0
someone <85572601+paulhuynh1607@users.noreply.github.com>
2024-11-27 12:43:24 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,6 +1,9 @@
 import pygame
+from GameLoadout import Background
+import random
+background = Background()
 
-SHAPES = [
+tetrominos = [
     [[1, 1, 1],
      [0, 1, 0]],  # T-shape
     [[1, 1],
@@ -19,24 +22,20 @@ SHAPES = [
 
 class Blocks:
     def __init__(self):
-        self.blockY = 29
-        self.blockX = 29
-        self.rect = pygame.Rect(150, 30, self.blockX, self.blockY)
+        self.random_block = 0
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, "blue", self.rect)
+    def draw(self):
+        shape = tetrominos[self.random_block]
+        background.grid[0][0] = 1
+        for i in shape:
+            for j in range(len(i)):
+                pass
 
     def falling(self, speed):
-        if self.rect.y != 570:
-            self.rect.move_ip(0, speed)
-        else:
-            self.rect.x = 150
-            self.rect.y = 30
+        pass
 
     def left(self, speed):
-        if self.rect.x != 30:
-            self.rect.move_ip(speed, 0)
+        pass
 
     def right(self, speed):
-        if self.rect.x != 270:
-            self.rect.move_ip(speed, 0)
+        pass
